0306d11485d42c052bfbe1fda37135070f5b97b9
MarinaArefjeva <130138294+MarinaArefjeva@users.noreply.github.com>
2023-08-04 13:25:44 +0200

    JavaScript

---
+++ script1.js
@@ -0,0 +1,90 @@
+//1)
+// let d = 7;
+// console.log(d);
+// console.log(d * d);
+// console.log(d * d * d);
+// //
+
+//2)
+
+// const val = promt();
+
+// if (val > 0) {
+//   console.log(val ** 2);
+
+//Если число положительное
+//}
+// 3)
+
+// const num1 = Number(prompt("Назовите число"));
+// const num2 = Number(prompt("Назовите число"));
+
+// if(num1 > num2){
+//     console.log(num1 + num2);
+// }   if(num1 < num2){
+//         console.log(num1 * num2);
+
+//     } else{
+//         console.log("Ziffern sind gleich");
+//     }
+
+// 4)
+// const num3 =Number(prompt("Nennen Sie eine Zahl"));
+
+// if (num3 < 0) {
+//     console.log(num3 * num3);
+// } else {
+//     console.log("Fehler");
+// }
+
+// 5)
+
+// const students = prompt("Anzahl der Schüller in der Klasse");
+// const chairs = prompt("Anzahl der Stühle im Klassenraum");
+
+// if (students < chairs); {
+//     console.log("genung Stühle");
+// }   if (students > chairs) {
+//     console.log("nicht genügend Stühle");
+// }
+
+// 6)
+
+// let count =["start, 1,2,3,4,5,6,7,8,9, finish"];
+// console.log(count);
+
+// 7)
+
+// for(let num = 10; num < 100; num++){
+//     if(num % 5 == 0)
+//     {
+//         console.log(num);
+//     }
+// }
+
+// 8)
+
+// let numbers = [12, 15, 30, 10, -13, 20, -3, 5, -22, 47];
+// for (let i = 0; i<numbers.length; i++){
+//     if(numbers[i]>=15){
+//         console.log(numbers[i]);
+//     }
+// }
+
+// 9)
+
+// for(let num = 0; num < 100; num++){
+//     if(num % 2 == 1)
+//     {
+//         console.log(num);
+//     }
+// }
+
+// 10)
+
+// let numbers = [12, 15, 30, 10, -13, 20, -3, 5, -22, 47];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 3 == 0) {
+//     console.log(numbers[i]);
+//   }
+// }
